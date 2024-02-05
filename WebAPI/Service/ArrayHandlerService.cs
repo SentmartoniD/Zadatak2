ce72d6e0c93d4cc7372a921f76494ec90f0e1079
Denes Sentmartoni <denessentmartoni.com>
2024-02-05 13:11:40 +0100

    Fixed the method deduplicate

--- WebAPI/Service/ArrayHandlerService.cs
+++ WebAPI/Service/ArrayHandlerService.cs
@@ -9,11 +9,12 @@ namespace WebAPI.Service
         static int counter = 0;
         public async Task<Output> Deduplicate(Int64[] data)
         {
-            HashSet<Int64> uniqueSet = new HashSet<Int64>();
+            HashSet<Int64> uniqueSet1 = new HashSet<Int64>();
+            HashSet<Int64> uniqueSet2 = new HashSet<Int64>();
             int indeks = 0, counter = 0;
             foreach (Int64 element in data)
             {
-                if (uniqueSet.Add(element))
+                if (uniqueSet1.Add(element))
                 {
                     ++counter;
                 }
@@ -21,7 +22,7 @@ namespace WebAPI.Service
             Int64[] result = new Int64[counter];
             foreach (Int64 element in data)
             {
-                if (uniqueSet.Add(element))
+                if (uniqueSet2.Add(element))
                 {
                     result[indeks++] = element;
                 }
